c2d3c6d602ffcfff6e9787d90b8c7191f8c56da9
Ritesh Kumar Shukla <riteshshuklagem@gmail.com>
2023-03-28 15:29:56 +0530

    completed navbar

--- fashionix/src/Components/HomePage/Navbar.jsx
+++ fashionix/src/Components/HomePage/Navbar.jsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import './Navbar.css';
+import { FaBars, FaSearch, FaShoppingCart, FaQuestion, FaUser } from 'react-icons/fa';
+import FashionIx from "../Images/FashionIx.png"
 
-const Navbar = () => {
+export default function Navbar() {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', changeWidth);
+
+    return () => {
+      window.removeEventListener('resize', changeWidth);
+    };
+  }, []);
+
   return (
-    <div>
-      NavBar
-    </div>
-  )
-}
+    <nav>
+      <div className="sideBar">
+        <FaBars size={30} /></div>
+      <div className="logo">
+        <img src={FashionIx} alt="Logo" />
+      </div>
 
-export default Navbar
+      <div className="search-box">
+        <input type="text" placeholder="SEARCH" className="searchInputBox" />
+
+      </div>
+
+      <div className="menu">
+        {screenWidth <= 500 && (
+          <button onClick={toggleNav} className="btn">
+            <FaBars />
+          </button>
+        )}
+        {(toggleMenu || screenWidth > 500) && (
+          <ul className="list">
+            <li className="items">
+              <a href="#"> <FaUser /></a>
+            </li>
+
+            <li className="items">
+              <a href="#">
+                <FaQuestion />
+              </a>
+            </li>
+
+            <li className="items">
+              <a href="#">
+                <FaShoppingCart />
+              </a>
+            </li>
+          </ul>
+        )}
+      </div>
+    </nav>
+  );
+}
