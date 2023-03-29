9c819169bff8239e3ec2bae3d782962719a0cda5
Jai raj singh <jairajsingh28july@gmail.com>
2023-03-29 22:27:18 +0530

    fw22_0038

--- fashionix/src/App.js
+++ fashionix/src/App.js
@@ -1,11 +1,14 @@
-import logo from "./logo.svg";
+import Footer from "./Components/footer/footer";
 import "./App.css";
 import Navbar from "./Components/Navbar";
-
+import { Login } from "./Components/loginsig/login";
+import { Register } from "./Components/register/register";
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Register/>
+      <Footer/>
     </div>
   );
 }
---
+++ fashionix/src/Components/loginsig/login.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./Login.css";
+export const Login = () => {
+  return (
+    <div 
+    className="mainDiv">
+        <div
+         className="innerDiv1"
+        >
+            <h5 >LOG IN TO YOUR ACCOUNT</h5>
+            <input 
+            className="form-Input"
+            placeholder='E-MAIL'/>
+            <br />
+            <input 
+            className="form-Input"
+            placeholder='PASSWORD'/>
+            <br />
+            <button
+            className="loginButton"
+            style={{width:"100%",padding:"10px",background:"black",color:"white",cursor:"pointer"}}
+            >LOGIN</button>
+            <p>Have you forgotten your password?</p>
+             </div>
+        <div
+        className="innerDiv2"
+        >
+            <h5>NEED AN ACCOUNT?</h5>
+            <button
+            style={{width:"100%",padding:"10px",border:"1px solid black",cursor:"pointer",marginTop:"10px"}}
+            >REGISTER</button>
+        </div>
+    </div>
+  )
+}
