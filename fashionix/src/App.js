f47effd580140a7890f5ed25f996424246d0edae
Ritesh Kumar Shukla <riteshshuklagem@gmail.com>
2023-03-29 11:51:33 +0530

    NavBar,CoverVideo,About Section Completed

--- fashionix/src/App.js
+++ fashionix/src/App.js
@@ -1,11 +1,27 @@
 import "./App.css";
 import Navbar from "./Components/HomePage/Navbar";
+import Loader from "./Components/HomePage/Loader";
+import React, { useState, useEffect } from "react";
+import { AnimatePresence } from "framer-motion";
+import Home from "./Components/HomePage/Home";
 
 function App() {
+
+  const [Loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 2000);
+  }, []);
+
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <>
+      <AnimatePresence>{Loaded ? null : <Loader />}</AnimatePresence>
+      <div className="App">
+        <Navbar />
+        <Home key="home" />
+      </div>
+    </>
   );
 }
 
--- fashionix/src/Components/HomePage/Navbar.jsx
+++ fashionix/src/Components/HomePage/Navbar.jsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import './Navbar.css';
-import { FaBars, FaSearch, FaShoppingCart, FaQuestion, FaUser } from 'react-icons/fa';
-import FashionIx from "../Images/FashionIx.png"
+import { FaBars, FaShoppingCart, FaQuestion, FaUser } from 'react-icons/fa';
+import FashionIx2 from "../Images/FashionIx2.png";
+import FashionIx from "../Images/FashionIx.png";
 
 export default function Navbar() {
   const [toggleMenu, setToggleMenu] = useState(false);
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [logoSrc, setLogoSrc] = useState('');
+  const [svgColor, setSvgColor] = useState('white');
+
 
   const toggleNav = () => {
     setToggleMenu(!toggleMenu);
@@ -15,25 +20,59 @@ export default function Navbar() {
     const changeWidth = () => {
       setScreenWidth(window.innerWidth);
     };
-
     window.addEventListener('resize', changeWidth);
-
     return () => {
       window.removeEventListener('resize', changeWidth);
     };
   }, []);
 
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const handleScroll = () => {
+    const scrollTop = window.pageYOffset;
+    if (scrollTop > 800) {
+      setIsScrolled(true);
+      setLogoSrc(FashionIx);
+      setSvgColor("black")
+    } else {
+      setIsScrolled(false);
+      setLogoSrc(FashionIx2);
+      setSvgColor("white")
+    }
+  };
+
+  const searchBoxClass = isScrolled ? 'searchInputBox scrolled' : 'searchInputBox';
+
+  //Sidebar
+  const [showSidebar, setShowSidebar] = useState(false);
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+    //Add the logic for sidebar open/close
+
+  };
+
+
   return (
-    <nav>
-      <div className="sideBar">
-        <FaBars size={30} /></div>
+    <nav className="navBar">
+      <div className="sideBar" onClick={toggleSidebar}>
+        <FaBars size={30} style={{ color: svgColor }} />
+      </div>
+
       <div className="logo">
-        <img src={FashionIx} alt="Logo" />
+        <img src={logoSrc} alt="Logo" />
       </div>
 
       <div className="search-box">
-        <input type="text" placeholder="SEARCH" className="searchInputBox" />
-
+        <input type="text" placeholder="SEARCH" className={searchBoxClass}
+          style={{
+            color: isScrolled ? 'black' : 'white',
+            borderBottom: isScrolled ? '1px solid black' : '1px solid white',
+          }} />
+        <style>{`.searchInputBox::placeholder { color: ${isScrolled ? 'black' : 'white'}; }`}</style>
       </div>
 
       <div className="menu">
@@ -45,18 +84,18 @@ export default function Navbar() {
         {(toggleMenu || screenWidth > 500) && (
           <ul className="list">
             <li className="items">
-              <a href="#"> <FaUser /></a>
+              <a href="#"> <FaUser style={{ color: svgColor }} /></a>
             </li>
 
             <li className="items">
               <a href="#">
-                <FaQuestion />
+                <FaQuestion style={{ color: svgColor }} />
               </a>
             </li>
 
             <li className="items">
               <a href="#">
-                <FaShoppingCart />
+                <FaShoppingCart style={{ color: svgColor }} />
               </a>
             </li>
           </ul>
