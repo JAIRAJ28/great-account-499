55ce1892f5666aa14eb8355f7cfc9f64e695c5db
Jai raj singh <jairajsingh28july@gmail.com>
2023-03-31 20:30:07 +0530

    JAIRAJ@

--- fashionix/src/App.js
+++ fashionix/src/App.js
@@ -5,9 +5,9 @@ import Loader from "./Components/HomePage/Loader";
 import React, { useState, useEffect } from "react";
 import { AnimatePresence } from "framer-motion";
 import Home from "./Components/HomePage/Home";
-import { Register } from "./Components/register/register";
+
 import Footer from "./Components/footer/footer";
-import Moredetails from "./Components/Singlepage/singledetails";
+
 function App() {
 
   const [Loaded, setLoaded] = useState(false);
