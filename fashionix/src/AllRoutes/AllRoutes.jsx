667a53f15199490f9f25066ac2ebcc0b295eb50a
Jeevan Jyoti Choudhury <jeevanjyoti549@gmail.com>
2023-04-02 10:03:43 +0530

    all routes updated

--- fashionix/src/AllRoutes/AllRoutes.jsx
+++ fashionix/src/AllRoutes/AllRoutes.jsx
@@ -1,19 +1,20 @@
 import React from "react";
-import Footer from "../Components/footer/footer"
+import Footer from "../Components/footer/footer";
 import { Route, Routes } from "react-router-dom";
 import Home from "../Components/HomePage/Home";
 import LeftSidebar from "../Components/LeftSidebar";
-import Women from "../Pages/ProductPages/Women"
-import Men from "../Pages/ProductPages/Men"
-import {Login} from "../Components/loginsig/login"
+import Women from "../Pages/ProductPages/Women";
+import Men from "../Pages/ProductPages/Men";
+import { Login } from "../Components/loginsig/login";
 import SearchBar from "../Pages/ProductPages/SearchBar";
 import CartandCheckout from "../Pages/Cart and Checkout/CartandCheckout";
-import { Register } from "../Components/register/register"
-import {Dashboard} from "../Admin/dashboard"
+import { Register } from "../Components/register/register";
+import Navbar from "../Components/HomePage/Navbar";
 
 const AllRoutes = () => {
   return (
     <div>
+      <Navbar />
       <LeftSidebar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -23,7 +24,6 @@ const AllRoutes = () => {
         <Route path="/cart" element={<CartandCheckout />} /> //private route
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/admin" element={<Dashboard />} /> //private route
       </Routes>
       <Footer />
     </div>
